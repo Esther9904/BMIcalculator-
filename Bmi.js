998db09e747bd69206db706ce5c05d599cbdaa25
Esther9904 <taiwoibeh@gmail.com>
2026-05-19 15:59:57 -0700

    Added next button function and name section

--- Bmi.js
+++ Bmi.js
@@ -1,3 +1,10 @@
+const NextButton = () => {
+    document.getElementById("step1").classList.remove("active");
+    document.getElementById("step2").classList.add("active");
+}
+
+
+
 function BodyMassIndex(){
     let weight = document.getElementById("weight").value;
     let height = document.getElementById("height").value;
