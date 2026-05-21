e90737368f7e939d9201d23ff385405320549570
Esther9904 <taiwoibeh@gmail.com>
2026-05-21 16:45:53 -0700

    Added check validity for email and alerts

--- Bmi.js
+++ Bmi.js
@@ -1,23 +1,23 @@
 const NextButton = () => {
     const name = document.getElementById("name").value;
     const message = document.getElementById("message");
+    const email = document.getElementById("email");
 
     const lettersOnly = /^[A-Za-z]+$/;
 
-    if (lettersOnly.test(name)) {
-        message.textContent = "Valid name!"
-        document.getElementById("step1").classList.remove("active");
-        document.getElementById("step2").classList.add("active");
-        const heading2 = document.querySelector('h2');
-        heading2.innerHTML = `WELCOME ${name}`;
-
-    } else{
-        message.textContent = "only letters are allowed. No space, numbers, or symbols.";
+    if (!lettersOnly.test(name)) {
+        alert("Only letters are allowed. No spaces, numbers or symbols.")
+        return;   
+    } 
+    if(!email.checkValidity()){
+        alert("Enter valid Email address")
+        return;
     }
 
-    
-    
-
+    const heading2 = document.querySelector('h2');
+    heading2.innerHTML = `WELCOME ${name}`;
+    document.getElementById("step1").classList.remove("active");
+    document.getElementById("step2").classList.add("active");
     
 }
 
