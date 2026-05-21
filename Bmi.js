c82fa068ad01d05f83e6c02a11d710a1c938d437
Esther9904 <taiwoibeh@gmail.com>
2026-05-21 04:16:55 -0700

    Added regex function for name input

--- Bmi.js
+++ Bmi.js
@@ -1,6 +1,24 @@
 const NextButton = () => {
-    document.getElementById("step1").classList.remove("active");
-    document.getElementById("step2").classList.add("active");
+    const name = document.getElementById("name").value;
+    const message = document.getElementById("message");
+
+    const lettersOnly = /^[A-Za-z]+$/;
+
+    if (lettersOnly.test(name)) {
+        message.textContent = "Valid name!"
+        document.getElementById("step1").classList.remove("active");
+        document.getElementById("step2").classList.add("active");
+        const heading2 = document.querySelector('h2');
+        heading2.innerHTML = `WELCOME ${name}`;
+
+    } else{
+        message.textContent = "only letters are allowed. No space, numbers, or symbols.";
+    }
+
+    
+    
+
+    
 }
 
 
